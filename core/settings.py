508ca54115e1343ae01465359536949bd651af8c
Rabin Dhamala <rabin.dhamala@aayulogic.com>
2021-11-30 15:56:50 +0545

    serializsers

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'result', 
     'crispy_forms',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- result/serializers.py
+++ result/serializers.py
@@ -1,1 +1,8 @@
-from result.models import Student, Subject, Teacher
+from result.models import Student, Subject, Teacher, Mark
+
+from rest_framework import serializers
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields=['rollno','teachers']
